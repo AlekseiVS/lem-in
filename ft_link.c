e5b5022a80685b5d2b7088ef530cbe57ac02b535
Oleksii SOKOLIUK <sokoliuk.av@gmail.com>
2018-05-30 10:00:13 +0300

    old_ft_search_link

--- ft_link.c
+++ ft_link.c
@@ -6,7 +6,7 @@
 /*   By: osokoliu <osokoliu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/15 14:57:12 by osokoliu          #+#    #+#             */
-/*   Updated: 2018/05/30 09:41:03 by osokoliu         ###   ########.fr       */
+/*   Updated: 2018/05/30 09:58:50 by osokoliu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,66 +65,67 @@ static void ft_entry_link(t_listlemin *tmp1, t_listlemin *tmp2)
     tmp1->link[i + 1] = 0;
 }
 
+static void ft_search_link(t_listlemin *tmp1, t_listlemin *tmp2, t_vertex vertex, int *count)
+{
+    while (tmp1)
+    {
+        if(ft_strequ(tmp1->name_room, vertex.u) == 1)
+        {
+            (*count)++;
+            while (tmp2)
+            {
+                if (ft_strequ(tmp2->name_room, vertex.v) == 1)
+                {
+                    (*count)++;
+                    break ;
+                }
+                tmp2 = tmp2->next;
+            }
+            ft_entry_link(tmp1, tmp2);
+            ft_entry_link(tmp2, tmp1);
+            break ;
+        }
+        tmp1 = tmp1->next;
+    }
+}
+
 int ft_link(char *line, t_listlemin **head)
 {
     t_vertex vertex;
     t_listlemin *tmp1;
     t_listlemin *tmp2;
     int count;
-    // int i;
 
+    tmp1 = *head;
+    tmp2 = *head;
     count = 0;
     if (ft_valid_link(line) == 1)
     {
         ft_vertex(line, &vertex);
-        tmp1 = *head;
-        while (tmp1)
-        {
-            if(ft_strequ(tmp1->name_room, vertex.u) == 1)
-            {
-                count++;
-                tmp2 = *head;
-                while (tmp2)
-                {
-                    if (ft_strequ(tmp2->name_room, vertex.v) == 1)
-                    {
-                        count++;
-                        break ;
-                    }
-                    tmp2 = tmp2->next;
-                }
-                ft_entry_link(tmp1, tmp2);
-                ft_entry_link(tmp2, tmp1);
-                // i = 0;
-                // if (tmp1->link)
-                // {
-                //     while(tmp1->link[i])
-                //         i++;
-                // }
-                // else
-                // {
-                //     tmp1->link = (t_listlemin**)malloc(sizeof(t_listlemin*));
-                // }
-                // tmp1->link[i] = (t_listlemin*)malloc(sizeof(t_listlemin) * 0);
-                // tmp1->link[i] = tmp2;
-                // tmp1->link[i + 1] = 0;
-                // i = 0;
-                // if (tmp2->link)
-                // {
-                //     while(tmp2->link[i])
-                //         i++;
-                // }
-                // else
-                // {
-                //     tmp2->link = (t_listlemin**)malloc(sizeof(t_listlemin*));
-                // }
-                // tmp2->link[i] = (t_listlemin*)malloc(sizeof(t_listlemin) * 0);
-                // tmp2->link[i] = tmp1;
-                // tmp2->link[i + 1] = 0;
-                break ;
-            }
-            tmp1 = tmp1->next;
-        }
+        ft_search_link(tmp1, tmp2, vertex, &count);
+        printf("%d\n", count);
+        // tmp1 = *head;
+        // while (tmp1)
+        // {
+        //     if(ft_strequ(tmp1->name_room, vertex.u) == 1)
+        //     {
+        //         count++;
+        //         tmp2 = *head;
+        //         while (tmp2)
+        //         {
+        //             if (ft_strequ(tmp2->name_room, vertex.v) == 1)
+        //             {
+        //                 count++;
+        //                 break ;
+        //             }
+        //             tmp2 = tmp2->next;
+        //         }
+        //         ft_entry_link(tmp1, tmp2);
+        //         ft_entry_link(tmp2, tmp1);
+        //         break ;
+        //     }
+        //     tmp1 = tmp1->next;
+        // }
         if (count == 2)
         {
             printf("%s\n", line);
