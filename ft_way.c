664f19c18f9c314bf8e6962877f7e94c40e3bc85
Oleksii SOKOLIUK <sokoliuk.av@gmail.com>
2018-05-30 11:25:40 +0300

    2_change_ft_way.c

--- ft_way.c
+++ ft_way.c
@@ -6,7 +6,7 @@
 /*   By: osokoliu <osokoliu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/24 12:30:19 by osokoliu          #+#    #+#             */
-/*   Updated: 2018/05/30 10:58:32 by osokoliu         ###   ########.fr       */
+/*   Updated: 2018/05/30 11:24:23 by osokoliu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,25 +37,62 @@ static void ft_start_queue(t_listlemin *tmp1, t_road **buff, t_road *new_node)
             i = 0;
             if (!new_node)
             {
-                new_node = (t_road*)malloc(sizeof(t_road));
-                new_node->next = NULL;
+                new_node = ft_create_elem_2();
                 *buff = new_node;
                 new_node->n_r = ft_strdup(tmp1->link[i]->name_room);
                 tmp1->link[i]->from = ft_strdup(tmp1->name_room);
-                // tmp1->link[i]->step = j;
                 i++;
                 while (tmp1->link[i])
                 {
-                    new_node->next = (t_road*)malloc(sizeof(t_road));
+                    new_node->next = ft_create_elem_2(); //(t_road*)malloc(sizeof(t_road));
                     new_node = new_node->next;
                     new_node->n_r = ft_strdup(tmp1->link[i]->name_room);
                     tmp1->link[i]->from = ft_strdup(tmp1->name_room);
-                    // tmp1->link[i]->step = j;
                     new_node->next = NULL;
                     i++;
                 }
             }
             break ;
+        }
+        tmp1 = tmp1->next;
+    }
+}
+
+static void ft_next_queue(t_listlemin **head, t_listlemin *tmp1, t_road *buff, t_road *new_node)
+{
+    int i;
+    t_road *buff2;
+
+    buff2 = buff;
+    while (tmp1)
+    {
+        if (!ft_strcmp(tmp1->name_room, (buff)->n_r))
+        {
+            tmp1->use = 1;
+            i = 0; 
+            while (tmp1->link[i])
+            {
+                while(buff2->next)
+                {
+                    buff2 = buff2->next;
+                }
+                if (tmp1->link[i]->use != 1 && ft_queue(buff, tmp1->link[i]->name_room) == 1)
+                {
+                    // j++;
+                    new_node = (t_road*)malloc(sizeof(t_road));
+                    new_node->next = NULL;
+                    new_node->n_r = ft_strdup(tmp1->link[i]->name_room);
+                    tmp1->link[i]->from = ft_strdup(tmp1->name_room);
+                    buff2->next = new_node;
+                    // tmp1->link[i]->step = j;
+                }
+                i++;
+            }
+            buff = (buff)->next;
+            if (tmp1->type_room == 2)
+                break ;
+            tmp1 = *head;
+            continue ;
         }
         tmp1 = tmp1->next;
     }
@@ -67,7 +104,7 @@ int ft_way(t_listlemin **head)
     t_listlemin *tmp1;
     t_road *new_node;
     t_road *buff;
-    t_road *buff2;
+    // t_road *buff2;
     t_road *buff3;
   
     
@@ -75,7 +112,7 @@ int ft_way(t_listlemin **head)
     new_node = 0;
     buff = new_node;
     
-    int i;
+    // int i;
     // int j = 1;
 
     ft_start_queue(tmp1, &buff, new_node);
@@ -109,42 +146,43 @@ int ft_way(t_listlemin **head)
     //     }
     //     tmp1 = tmp1->next;
     // }
-    tmp1 = *head;
+    // tmp1 = *head;
     buff3 = buff;
-    buff2 = buff;
+    // buff2 = buff;
     
-    while (tmp1)
-    {
-        if (!ft_strcmp(tmp1->name_room, buff->n_r))
-        {
-            tmp1->use = 1;
-            i = 0; 
-            while (tmp1->link[i])
-            {
-                while(buff2->next)
-                {
-                    buff2 = buff2->next;
-                }
-                if (tmp1->link[i]->use != 1 && ft_queue(buff, tmp1->link[i]->name_room) == 1)
-                {
-                    // j++;
-                    new_node = (t_road*)malloc(sizeof(t_road));
-                    new_node->next = NULL;
-                    new_node->n_r = ft_strdup(tmp1->link[i]->name_room);
-                    tmp1->link[i]->from = ft_strdup(tmp1->name_room);
-                    buff2->next = new_node;
-                    // tmp1->link[i]->step = j;
-                }
-                i++;
-            }
-            buff = buff->next;
-            if (tmp1->type_room == 2)
-                break ;
-            tmp1 = *head;
-            continue ;
-        }
-        tmp1 = tmp1->next;
-    }
+    ft_next_queue(head, tmp1, buff, new_node);
+    // while (tmp1)
+    // {
+    //     if (!ft_strcmp(tmp1->name_room, buff->n_r))
+    //     {
+    //         tmp1->use = 1;
+    //         i = 0; 
+    //         while (tmp1->link[i])
+    //         {
+    //             while(buff2->next)
+    //             {
+    //                 buff2 = buff2->next;
+    //             }
+    //             if (tmp1->link[i]->use != 1 && ft_queue(buff, tmp1->link[i]->name_room) == 1)
+    //             {
+    //                 // j++;
+    //                 new_node = (t_road*)malloc(sizeof(t_road));
+    //                 new_node->next = NULL;
+    //                 new_node->n_r = ft_strdup(tmp1->link[i]->name_room);
+    //                 tmp1->link[i]->from = ft_strdup(tmp1->name_room);
+    //                 buff2->next = new_node;
+    //                 // tmp1->link[i]->step = j;
+    //             }
+    //             i++;
+    //         }
+    //         buff = buff->next;
+    //         if (tmp1->type_room == 2)
+    //             break ;
+    //         tmp1 = *head;
+    //         continue ;
+    //     }
+    //     tmp1 = tmp1->next;
+    // }
     while (buff3)
     {
         printf("%s", buff3->n_r);
